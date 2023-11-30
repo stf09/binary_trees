f92c915eb6368c9c9f7f3f4e2739e4f8284b8406
stf09 <bikemlit32@gmail.com>
2023-11-30 12:39:48 +0000

    3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,8 +1,11 @@
 #include "binary_trees.h"
+#include "stdlib.h"
+
 /**
 * binary_tree_delete - inserts a node as the left-child of another node
 * @tree : pointer to root node
 */
+
 void binary_tree_delete(binary_tree_t *tree)
 {
 	if (tree)
